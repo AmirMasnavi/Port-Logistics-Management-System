dea0e40c0a4f45e000f1b46fa3a6a09223271f85
leonor marinho <1230977@isep.ipp.pt>
2025-10-13 10:04:58 +0100

    US2.2.5: implementation-creat OrganizationId.cs (#54 & 51)

---
+++ PortProject.Api/Domain/ShippingAgentOrganizationAggregate/OrganizationId.cs
@@ -0,0 +1,44 @@
+using System;
+
+namespace PortProject.Api.Domain.ShippingAgentOrganizationAggregate
+{
+
+    public sealed class OrganizationId : IEquatable<OrganizationId>
+    {
+        public Guid Value { get; }
+
+        private OrganizationId() { } // para EF Core
+
+        public OrganizationId(Guid value)
+        {
+            if (value == Guid.Empty)
+                throw new ArgumentException("OrganizationId cannot be empty", nameof(value));
+
+            Value = value;
+        }
+
+        // Factory method útil
+        public static OrganizationId NewId() => new OrganizationId(Guid.NewGuid());
+
+        #region Equality Members
+        public bool Equals(OrganizationId other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return Value.Equals(other.Value);
+        }
+
+        public override bool Equals(object obj) => Equals(obj as OrganizationId);
+
+        public override int GetHashCode() => Value.GetHashCode();
+
+        public static bool operator ==(OrganizationId left, OrganizationId right) =>
+            Equals(left, right);
+
+        public static bool operator !=(OrganizationId left, OrganizationId right) =>
+            !Equals(left, right);
+        #endregion
+
+        public override string ToString() => Value.ToString();
+    }
+}
