d74e760cff253db78770c4d337876a915c22864b
leonor marinho <1230977@isep.ipp.pt>
2025-10-13 10:40:12 +0100

    US2.2.5: implementation-create OrganizationDto.cs (#59)

---
+++ PortProject.Api/Application/ShippingAgentsOrganization/DTOs/OrganizationDto.cs
@@ -0,0 +1,13 @@
+
+namespace PortProject.Api.Application.ShippingAgentsOrganization.DTOs
+{
+    public sealed class OrganizationDto
+    {
+        public Guid Id { get; init; }
+        public string LegalName { get; init; } = default!;
+        public string AlternativeName { get; init; } = default!;
+        public string Address { get; init; } = default!;
+        public string TaxNumber { get; init; } = default!;
+        public List<RepresentativeItemDto> Representatives { get; init; } = new();
+    }
+}
