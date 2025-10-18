0f4dea6ca2dc512dc20aef9e25afc7cc53ad6640
Mariana Sarrmento <1231039@isep.ipp.pt>
2025-10-18 18:47:06 +0100

    US2.2.7: feat - service,repository, domain(#17)

---
+++ PortProject.Api/Application/VesselVisitNotification/DTOs/ApproveVvnDto.cs
@@ -0,0 +1,7 @@
+﻿namespace PortProject.Api.Application.VesselVisitNotification.DTOs;
+
+public class ApproveVvnDto
+{
+    public string OfficerId { get; set; }
+    public string DockId { get; set; }
+}
