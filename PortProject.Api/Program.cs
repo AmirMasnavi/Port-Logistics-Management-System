b458a872929a807b3f1da778ae26c4199fd65c04
PedroS42 <1221431@isep.ipp.pt>
2025-10-30 11:10:21 +0000

    US3.1.1: feat - SPA setted up and working correctly with endpoints (#124)

--- PortProject.Api/Program.cs
+++ PortProject.Api/Program.cs
@@ -76,8 +76,18 @@ builder.Services.AddScoped<IResourceService, ResourceService>();
 builder.Services.AddScoped<IResourceRepository, ResourceRepository>();
 
 
+// Add CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend", policy => policy.WithOrigins("http://localhost:3000")
+        .AllowAnyHeader()
+        .AllowAnyMethod());
+});
+
+
 var app = builder.Build();
 
+app.UseCors("AllowFrontend");
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
---
+++ port-spa-app/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import VesselTypesPage from './pages/VesselTypesPage.js';
+
+function App() {
+  return (
+      <VesselTypesPage/>
+  );
+}
+
+export default App;
