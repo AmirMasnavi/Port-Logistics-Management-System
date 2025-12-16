f65cfa082c6d005649735a761ee8a0455c0779f1
Ines Gomes <1231205@isep.ipp.pt>
2025-12-16 11:43:03 +0000

    US4.5.3- fix: fix build bug related with DataRightsPage.tsx

--- port-spa-app/src/pages/DataRightsPage.tsx
+++ port-spa-app/src/pages/DataRightsPage.tsx
@@ -103,9 +103,7 @@ const DataRightsPage: React.FC = () => {
         switch (status) {
             case RequestStatus.Pending:
                 return 'bg-yellow-100 text-yellow-800';
-            case RequestStatus.Approved:
-                return 'bg-blue-100 text-blue-800';
-            case RequestStatus.Completed:
+            case RequestStatus.Processed:
                 return 'bg-green-100 text-green-800';
             case RequestStatus.Rejected:
                 return 'bg-red-100 text-red-800';
